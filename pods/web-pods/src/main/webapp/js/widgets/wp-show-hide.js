8f956476212abfe5be623a0e86d35dee8a87ba78
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2016-02-29 15:05:45 -0500

    web-pods: wp-show-hide component added

---
+++ pods/web-pods/src/main/webapp/js/widgets/wp-show-hide.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    
+    // WebPODS show/hide roots, indexed by id
+    var wpShowHides = {};
+    
+    function showHide(showHideId, value) {
+        var query = "#" + showHideId;
+        if (value) {
+            switch(value.type.name) {
+                case "VTable":
+                    $(query).show();
+                    break;
+                default:
+                    if (value.value) {
+                        $(query).show();
+                    } else {
+                        $(query).hide();
+                    }
+                    break;
+            }
+        } else {
+            $(query).hide();
+        }
+    }
+        
+    var createChannelCallback = function (showHideId) {
+        return function (evt, channel) {
+            switch (evt.type) {
+                case "connection": //connection state changed
+                    break;
+                case "value": //value changed
+                    showHide(showHideId, evt.value);
+                    break;
+                case "error": //error happened
+                    break;
+                case "writePermission":	// write permission changed.
+                    break;
+                case "writeCompleted": // write finished.
+                    break;
+                default:
+                    break;
+            }
+        };
+    };
+    
+    var nodes = document.getElementsByClassName("wp-show-hide");
+    var counter = 0;
+    for (var i = 0; i < nodes.length; i++) {
+        var channelName = nodes[i].getAttribute("data-channel");
+        var showHideId = nodes[i].getAttribute("id");
+        if (showHideId === null) {
+            counter++;
+            showHideId = "show-hide-" + counter;
+            nodes[i].id = showHideId;
+        }
+        
+        if (channelName !== null && channelName.trim().length > 0) {
+            var channel = wp.subscribeChannel(channelName, createChannelCallback(showHideId), true);
+        }
+    }
+});
+
+window.onbeforeunload = function () {
+    wp.close();
+};
+
