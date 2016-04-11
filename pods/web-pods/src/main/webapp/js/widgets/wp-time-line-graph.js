c80053d5b855788dd2f6e2c847cf581a7254d2ae
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2016-04-11 15:07:05 -0400

    pods.web: rewrote time-line-graph in a more OO style

--- pods/web-pods/src/main/webapp/js/widgets/wp-time-line-graph.js
+++ pods/web-pods/src/main/webapp/js/widgets/wp-time-line-graph.js
@@ -1,96 +1,96 @@
+function WpTimeLineGraph(node) {
+    var self = this;
+    var root;
+    var channelName = "what?";
+    var chart;
+    var id;
+    var timestampColumn = 0;
+    var channel;
+    
+    this.setValue = function(value) {
+        for (var valuesColumn = 0; valuesColumn < value.columnNames.length; valuesColumn++) {
+            if (valuesColumn === timestampColumn) {
+                continue;
+            }
+            var seriesID = value.columnNames[valuesColumn];
+            var data = [];
+            var nPoints = value.columnValues[valuesColumn].length;
+            for (var i=0; i < nPoints; i++) {
+                data[i] = [value.columnValues[timestampColumn][i], value.columnValues[valuesColumn][i]];
+            }
+
+            var series = chart.get(seriesID);
+            if (series) {
+                series.setData(data, true, false, false);
+            } else {
+                chart.setTitle({text : null}, {}, true);
+                chart.addSeries({
+                        name : seriesID,
+                        id : seriesID,
+                        data : data,
+                        type : 'line',
+                        threshold : null,
+                        tooltip : {
+                            valueDecimals : 2
+                        }
+                    },true, false);
+            }
+        }
+    };
+    
+    var callback = function (evt, channel) {
+        switch (evt.type) {
+            case "connection": //connection state changed
+                break;
+            case "value": //value changed
+                self.setValue(evt.value);
+                break;
+            case "error": //error happened
+                break;
+            case "writePermission":	// write permission changed.
+                break;
+            case "writeCompleted": // write finished.
+                break;
+            default:
+                break;
+        }
+    };
+
+    // Constructor
+    root = node;
+    if (!root.id) {
+        WpTimeLineGraph.counter++;
+        root.id = "wp-time-line-" + WpTimeLineGraph.counter;
+    }
+    id = root.id;
+    WpTimeLineGraph.widgets[id] = this;
+
+    channelName = root.getAttribute("data-channel");
+
+    // Create the chart
+    $('#' + id).highcharts('StockChart', {
+
+        title : {
+            text : 'Waiting for data'
+        }
+    });
+    chart = $('#' + id).highcharts();
+
+    // Subscribe to the channel
+    channel = wp.subscribeChannel(channelName, callback, true);
+}
+
+// Keep a list of widgets
+WpTimeLineGraph.widgets = {};
+
+// Used to create unique ids
+WpTimeLineGraph.counter = 0;
+
+// Create widgets
 $(document).ready(function () {
     var nodes = document.getElementsByClassName("wp-time-line-graph");
-    var len = nodes.length;
-    var charts = {};
-    var channels = {};
-    var values = {};
-    var selectX = {};
-    var selectY = {};
-    var selectColor = {};
-    var graphDivs = {};
-    counter = 0;
 
-    for (var i = 0; i < len; i++) {
-        // Extract the node and all its properties
-        var masterDiv = nodes[i];
-        var id = nodes[i].getAttribute("id");
-        if (id === null) {
-            counter++;
-            id = "wp-time-line-" + counter;
-            nodes[i].id = id;
-        }
-        var channelname = nodes[i].getAttribute("data-channel");
-        var xColumn = nodes[i].getAttribute("data-x-column");
-        var yColumn = nodes[i].getAttribute("data-y-column");
-        var colorColumn = nodes[i].getAttribute("data-color-column");
-        var readOnly = "true";
-        
-        // Create the chart
-        $('#' + id).highcharts('StockChart', {
-
-            title : {
-                text : 'Waiting for data'
-            },
-        });
-        charts[i] = $('#' + id).highcharts();
-        
-        var processValue = function (channel, nNode) {
-            var value = values[channel.getId()];
-            
-            var timestampColumn = 0;
-            
-            for (var valuesColumn = 0; valuesColumn < value.columnNames.length; valuesColumn++) {
-                if (valuesColumn === timestampColumn) {
-                    continue;
-                }
-                var seriesID = value.columnNames[valuesColumn];
-                var data = [];
-                var nPoints = value.columnValues[valuesColumn].length;
-                for (var i=0; i < nPoints; i++) {
-                    data[i] = [value.columnValues[timestampColumn][i], value.columnValues[valuesColumn][i]];
-                }
-
-                var series = charts[nNode].get(seriesID);
-                if (series) {
-                    series.setData(data, true, false, false);
-                } else {
-                    charts[nNode].setTitle({text : null}, {}, true);
-                    charts[nNode].addSeries({
-                            name : seriesID,
-                            id : seriesID,
-                            data : data,
-                            type : 'line',
-                            threshold : null,
-                            tooltip : {
-                                valueDecimals : 2
-                            }
-                        },true, false);
-                }
-            }
-        };
-        
-        var createCallback = function (nNode) {
-
-            return function (evt, channel) {
-                switch (evt.type) {
-                    case "connection": //connection state changed
-                        break;
-                    case "value": //value changed
-                        values[channel.getId()] = evt.value;
-                        processValue(channel, nNode);
-                        break;
-                    case "error": //error happened
-                        break;
-                    case "writePermission":	// write permission changed.
-                        break;
-                    case "writeCompleted": // write finished.
-                        break;
-                    default:
-                        break;
-                }
-            };
-        };
-        
-        channels[i] = wp.subscribeChannel(channelname, createCallback(i), readOnly);
+    for (var i = 0; i < nodes.length; i++) {
+        new WpTimeLineGraph(nodes[i]);
     }
 });
