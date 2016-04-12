32cf899689c3bb93cbee8cf0c1ca6e3629b332e9
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2016-04-12 12:51:56 -0400

    pods.web: refactored wp-time-area-graph

--- pods/web-pods/src/main/webapp/js/widgets/wp-time-area-graph.js
+++ pods/web-pods/src/main/webapp/js/widgets/wp-time-area-graph.js
@@ -1,50 +1,16 @@
-$(document).ready(function () {
-    var nodes = document.getElementsByClassName("wp-time-area-graph");
-    var len = nodes.length;
-    var charts = {};
-    var channels = {};
-    var values = {};
-    var selectX = {};
-    var selectY = {};
-    var selectColor = {};
-    var graphDivs = {};
-    counter = 0;
-
-    for (var i = 0; i < len; i++) {
-        // Extract the node and all its properties
-        var masterDiv = nodes[i];
-        var id = nodes[i].getAttribute("id");
-        if (id === null) {
-            counter++;
-            id = "wp-time-area-" + counter;
-            nodes[i].id = id;
-        }
-        var channelname = nodes[i].getAttribute("data-channel");
-        var xColumn = nodes[i].getAttribute("data-x-column");
-        var yColumn = nodes[i].getAttribute("data-y-column");
-        var colorColumn = nodes[i].getAttribute("data-color-column");
-        var readOnly = "true";
-        
-        // Create the chart
-        $('#' + id).highcharts('StockChart', {
-
-            title : {
-                text : 'Waiting for data'
-            },
-            
-            plotOptions: {
-                area: {
-                    stacking: 'normal'
-                }
-            }
-        });
-        charts[i] = $('#' + id).highcharts();
-        
-        var processValue = function (channel, nNode) {
-            var value = values[channel.getId()];
-            
-            var timestampColumn = 0;
-            
+function WpTimeAreaGraph(node) {
+    var self = this;
+    var root;
+    var channelName;
+    var clearChannelName;
+    var chart;
+    var id;
+    var timestampColumn = 0;
+    var channel;
+    var clearChannel;
+    
+    this.setValue = function(value) {
+        if (value) {
             for (var valuesColumn = 0; valuesColumn < value.columnNames.length; valuesColumn++) {
                 if (valuesColumn === timestampColumn) {
                     continue;
@@ -56,12 +22,12 @@ $(document).ready(function () {
                     data[i] = [value.columnValues[timestampColumn][i], value.columnValues[valuesColumn][i]];
                 }
 
-                var series = charts[nNode].get(seriesID);
+                var series = chart.get(seriesID);
                 if (series) {
                     series.setData(data, true, false, false);
                 } else {
-                    charts[nNode].setTitle({text : null}, {}, true);
-                    charts[nNode].addSeries({
+                    chart.setTitle({text : null}, {}, true);
+                    chart.addSeries({
                             name : seriesID,
                             id : seriesID,
                             data : data,
@@ -71,33 +37,104 @@ $(document).ready(function () {
                                 valueDecimals : 2
                             }
                         },false, false);
+                    chart.redraw();
                 }
-                charts[nNode].redraw();
             }
-        };
-        
-        var createCallback = function (nNode) {
+        } else {
+            while(chart.series.length > 0) {
+                chart.series[0].remove(true);
+            }
+            initChart();
+        }
+    };
+    
+    var channelCallback = function (evt, channel) {
+        switch (evt.type) {
+            case "connection": //connection state changed
+                break;
+            case "value": //value changed
+                self.setValue(evt.value);
+                break;
+            case "error": //error happened
+                break;
+            case "writePermission":	// write permission changed.
+                break;
+            case "writeCompleted": // write finished.
+                break;
+            default:
+                break;
+        }
+    };
+    
+    var clearChannelCallback = function (evt, channel) {
+        switch (evt.type) {
+            case "connection": //connection state changed
+                break;
+            case "value": //value changed
+                self.setValue(null);
+                break;
+            case "error": //error happened
+                break;
+            case "writePermission":	// write permission changed.
+                break;
+            case "writeCompleted": // write finished.
+                break;
+            default:
+                break;
+        }
+    };
+    
+    var initChart = function() {
+        $('#' + id).highcharts('StockChart', {
 
-            return function (evt, channel) {
-                switch (evt.type) {
-                    case "connection": //connection state changed
-                        break;
-                    case "value": //value changed
-                        values[channel.getId()] = evt.value;
-                        processValue(channel, nNode);
-                        break;
-                    case "error": //error happened
-                        break;
-                    case "writePermission":	// write permission changed.
-                        break;
-                    case "writeCompleted": // write finished.
-                        break;
-                    default:
-                        break;
-                }
-            };
-        };
+            title : {
+                text : 'Waiting for data'
+            },
         
-        channels[i] = wp.subscribeChannel(channelname, createCallback(i), readOnly);
+            plotOptions: {
+                area: {
+                    stacking: 'normal'
+                }
+            }
+        });
+        chart = $('#' + id).highcharts();
+    };
+
+    // Constructor
+    root = node;
+    if (!root.id) {
+        WpTimeAreaGraph.counter++;
+        root.id = "wp-time-area-" + WpTimeAreaGraph.counter;
+    }
+    id = root.id;
+    WpTimeAreaGraph.widgets[id] = this;
+
+    channelName = root.getAttribute("data-channel");
+    clearChannelName = root.getAttribute("data-clear-channel");
+
+    // Create the chart
+    initChart();
+    
+    // Subscribe to the channel
+    channel = WebPodsClient.client.subscribeChannel(channelName, channelCallback, true);
+    
+    if (clearChannelName) {
+        // Subscribe to the clearchannel
+        clearChannel = WebPodsClient.client.subscribeChannel(clearChannelName, clearChannelCallback, true);
+    }
+}
+
+// Keep a list of widgets
+WpTimeAreaGraph.widgets = {};
+
+// Used to create unique ids
+WpTimeAreaGraph.counter = 0;
+
+// Create widgets
+$(document).ready(function () {
+    var nodes = document.getElementsByClassName("wp-time-area-graph");
+
+    for (var i = 0; i < nodes.length; i++) {
+        new WpTimeAreaGraph(nodes[i]);
     }
 });
